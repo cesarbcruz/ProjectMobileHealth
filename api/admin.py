8c4a08af3bb0f5e098faf7f7909077ccc334973b
cesar <you@example.com>
2017-03-06 15:23:41 -0300

    modulo server rest location

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Monitoring
 from geopy.geocoders import GoogleV3
 from django.utils.html import format_html
+import os
 
 class MonitoringAdmin(admin.ModelAdmin):
     def show_location(self, obj):
@@ -12,23 +13,25 @@ class MonitoringAdmin(admin.ModelAdmin):
     show_location.short_description = 'Localização'
     show_location.allow_tags = True
 
-    # def show_address(self, obj):
-    #     geolocator = GoogleV3()
-    #     location = None
-    #     if obj.location():
-    #         location = geolocator.reverse(obj.location())
-    #     if location:
-    #         address = ''
-    #         for l in location:
-    #             address += l.address + "\n"
-    #         return format_html("<textarea readonly>" + address + "</textarea>")
-    #     else:
-    #         return 'Não identificado'
-    # show_address.short_description = 'Endereço aproximado '
-    # show_address.allow_tags = True
-    # 'show_address'
+    def show_address(self, obj):
+        geolocator = GoogleV3(scheme="http")
+        os.environ['http_proxy'] = os.environ['QUOTAGUARD_URL']
 
-    list_display = ('user', 'date_time', 'heart_rate', 'show_location', )
+        location = None
+        if obj.location():
+            location = geolocator.reverse(obj.location())
+        if location:
+            address = ''
+            for l in location:
+                address += l.address + "\n"
+            return format_html("<textarea readonly>" + address + "</textarea>")
+        else:
+            return 'Não identificado'
+    show_address.short_description = 'Endereço aproximado '
+    show_address.allow_tags = True
+
+
+    list_display = ('user', 'date_time', 'heart_rate', 'show_location', 'show_address')
 
 
 
