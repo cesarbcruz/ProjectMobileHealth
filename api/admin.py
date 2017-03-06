055d3f5094931338a76be9e8219a6ae4e875c098
cesar <you@example.com>
2017-03-06 15:31:51 -0300

    modulo server rest location

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Monitoring
 from geopy.geocoders import GoogleV3
 from django.utils.html import format_html
-import os
 
 class MonitoringAdmin(admin.ModelAdmin):
     def show_location(self, obj):
@@ -14,22 +13,23 @@ class MonitoringAdmin(admin.ModelAdmin):
     show_location.allow_tags = True
 
     def show_address(self, obj):
-        geolocator = GoogleV3(scheme="http")
-        os.environ['http_proxy'] = os.environ['QUOTAGUARD_URL']
-
-        location = None
-        if obj.location():
-            location = geolocator.reverse(obj.location())
-        if location:
-            address = ''
-            for l in location:
-                address += l.address + "\n"
-            return format_html("<textarea readonly>" + address + "</textarea>")
-        else:
-            return 'Não identificado'
-    show_address.short_description = 'Endereço aproximado '
+        try:
+            geolocator = GoogleV3()
+            location = None
+            if obj.location():
+                location = geolocator.reverse(obj.location())
+            if location:
+                address = ''
+                for l in location:
+                    address += l.address + "\n"
+                return format_html("<textarea readonly>" + address + "</textarea>")
+            else:
+                return 'Não identificado'
+        except Exception as e:
+            return  "Erro: {0}".format(e)
+        
+    show_address.short_description = 'Endereço aproximado'
     show_address.allow_tags = True
-
 
     list_display = ('user', 'date_time', 'heart_rate', 'show_location', 'show_address')
 
